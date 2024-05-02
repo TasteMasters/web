e47ea53ef3cd1893ca9ab03730a88640970ad6e4
Pedrosávio <pedrosaviolima618@gmail.com>
2024-05-02 15:58:19 -0300

    responsive footer

--- assets/js/components/footer.js
+++ assets/js/components/footer.js
@@ -2,7 +2,7 @@ export default function footer() {
     const template = `
         <section id="about-section">
             <div>
-                <img src="/assets/img/logo.svg" alt="Logo TasteMaster" />
+                <img src="/assets/img/logo.svg" alt="Logo TasteMaster" id="img-footer-logo"/>
                 <p>Siga a TasteMaster no Facebook, Instagram e Twitter para se juntar à nossa comunidade culinária vibrante e inspiradora.</p>
             </div>
             <div>
@@ -14,9 +14,9 @@ export default function footer() {
             </div>
         </section>
         <section id="icons-section">
-            <a href="link"><img src="/assets/img/icon-facebook.svg" alt="Ícone do Facebook" class="icons"  id="icon-facebook" /></a>
-            <a href="link"><img src="/assets/img/icon-instagram.svg" alt="Ícone do Instagram" class="icons" id="icon-instagram" /></a>
-            <a href="link"><img src="/assets/img/icon-twitter.svg" alt="Ícone do Twitter" class="icons" id="icon-twitter" /></a>
+            <a href="/"><img src="/assets/img/icon-facebook.svg" alt="Ícone do Facebook" class="icons"  id="icon-facebook" /></a>
+            <a href="/"><img src="/assets/img/icon-instagram.svg" alt="Ícone do Instagram" class="icons" id="icon-instagram" /></a>
+            <a href="/"><img src="/assets/img/icon-twitter.svg" alt="Ícone do Twitter" class="icons" id="icon-twitter" /></a>
             <hr />
         </section>
         <p id="direitos-tastemaster">Direitos reservados à TasteMaster</p>
